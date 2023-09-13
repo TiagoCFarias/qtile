10385853daea7a460ca568bfc6c52b1cc96c6398
Tiago <143773064+TiagoCFarias@users.noreply.github.com>
2023-09-12 23:32:25 -0400

    Remove catppuccin color config from config and add to its own file

--- qtile/config.py
+++ qtile/config.py
@@ -42,26 +42,6 @@ mod = "mod4"
 
 
 
-# COLORS FOR THE BAR
-catppuccin = {
-    "flamingo": "#F2CDCD",
-    "mauve": "#DDB6F2",
-    "pink": "#F5C2E7",
-    "maroon": "#E8A2AF",
-    "red": "#F28FAD",
-    "peach": "#F8BD96",
-    "yellow": "#FAE3B0",
-    "green": "#ABE9B3",
-    "teal": "#B5E8E0",
-    "blue": "92CDFB",
-    "sky": "#89DCEB",
-    "white": "#D9E0EE",
-    "gray0": "#6E6C7E",
-    "black1": "#1A1826",
-}
-
-
-
 # groups = [Group(i) for i in "1234567890"]
 
 
