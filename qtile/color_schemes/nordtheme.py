dc9347f9b13b27ececa597f7087c02b27a26c6b4
Tiago <143773064+TiagoCFarias@users.noreply.github.com>
2023-09-15 12:50:33 -0400

    Add nordic color scheme

---
+++ qtile/color_schemes/nordtheme.py
@@ -0,0 +1,27 @@
+polar_night = [
+    '#2e3443',
+    '#3b4255',
+    '#434c5e',
+    '#4c566a',
+]
+
+snow_storm = [
+    '#d8dee9',
+    '#e5e9f0',
+    '#eceff4',
+]
+
+frost = [
+    '#8fbcbb',
+    '#88c0d0',
+    '#81a1c1',
+    '#5e81ac',
+]
+
+aurora = [
+    '#bf616a',
+    '#d08770',
+    '#ebcb8b',
+    '#a3be8c',
+    '#b48ead',
+]
